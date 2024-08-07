73e3c4ebab4a2fc8a5983f899cab981d9e491983
Sharyar Anwer <76113895+MUSHARIB-97@users.noreply.github.com>
2024-08-07 13:10:20 -0700

    new component

--- src/pages/layout/Index.jsx
+++ src/pages/layout/Index.jsx
@@ -1,74 +1,108 @@
-import React, { useState } from 'react';
-import {
- 
-  PieChartOutlined,
-  
-} from '@ant-design/icons';
-import { Layout, Menu, theme } from 'antd';
-import colors from '../../assets/colors/color';
-import './Index.css'
+import React, { useState } from "react";
 
+import { Layout } from "antd";
+import colors from "../../assets/colors/color";
+import "./Index.css";
+import icons from "../../assets/icons";
+import { CardsSection } from "../../components/cardsSection/CardsSection";
+import Popup from "../../assets/select/Popup";
+import { FilterComp } from "../../assets/others/Others";
 
-const { Header, Content, Footer, Sider } = Layout;
-function getItem(label, key, icon, children) {
-  return {
-    key,
-    icon,
-    children,
-    label,
-  };
-}
-const items = [
-  getItem('Option 1', '1', <PieChartOutlined />),
-  
-];
+const {Content, Sider } = Layout;
+
 const Index = () => {
   const [collapsed, setCollapsed] = useState(false);
-  const {
-    token: { colorBgContainer, borderRadiusLG },
-  } = theme.useToken();
+ 
   return (
     <Layout
       style={{
-        minHeight: '100vh',
-        backgroundColor:colors.theme 
+        minHeight: "80vh",
+        backgroundColor: colors.theme,
       }}
     >
-      <Sider  collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+      <Sider width={!collapsed ? "250" : "20"} collapsed={collapsed} className="borderClass">
         <div className="demo-logo-vertical" />
-        <Menu theme={colors.theme} className='border ' defaultSe lectedKeys={['1']} mode="inline" items={items} />
+        <div className="sidebar-options  ">
+          {!collapsed ? (
+            <div className="menueClass d-flex ">
+              <div class="BVceZHOoUszsgw r6KV0yEdmnh3Op">T</div>
+
+              <div className=" text-white text-class  my-1 px-2 d-flex ">
+                Staffshaw Workspace <br />
+                free
+                <div
+                  className="m-1 my-3 cursor-pointer"
+                  onClick={(value) => setCollapsed(value)}
+                >
+                  {icons.sidebarclose}
+                </div>
+              </div>
+            </div>
+          ) : (
+            <div
+              className="m-1  my-3 d-flex position-relative justify-content-end  text-white cursor-pointer"
+              style={{ cursor: "pointer", left: "10px" }}
+              onClick={(value) => setCollapsed(!value)}
+            >
+              {icons.sidebarOpen}
+            </div>
+          )}
+        </div>
       </Sider>
       <Layout>
-        <Header
+        <div
           style={{
-            padding: 0,
-            background: colorBgContainer,
-          }}
-        />
-        <Content
-          style={{
-            margin: '0 16px',
+            padding: 16,
+
+            color: "white",
+            background: colors.darkTheme,
           }}
         >
-          
+          <div className="headesss d-flex justify-content-between">
+            <div className="fs-6 fw-bolder ">
+              <span className="p-2 styleButton">Project Name</span>
+              <span className=" mx-1 p-2 text-white styleButton">
+                {icons.starIcon}
+              </span>
+              <span className=" mx-1 p-2 text-white styleButton">
+                {icons.peopleGroupIcon}
+              </span>
+              <span className=" mx-1 text-white p-2 rounded-1   styleButton">
+                <span>
+                  {icons.listIcon}
+                  <span className="mx-2">Board</span>
+                  <span>{icons.downIcon}</span>
+                </span>
+              </span>
+            </div>
+            <div className="filter-Section">
+          <div className="filterBox text-white d-flex p-1 px-2 justify-content-between   styleButton"  >
+            <div className="filterIcon mx-2 ">
+                    <Popup title="Filters" className='text-white bg-body p-0 bg-transparent border-0' icon={icons.filterIcon} component={<FilterComp  icon={icons.peopleGroupIcon} />} />
+            </div>
+            <div className="filterTittle fw-bolder">
+              Filter
+            </div>
+          </div>
+
+            
+            </div>
+          </div>{" "}
+        </div>
+        <Content
+         style={{backgroundColor:colors.theme, width:"auto" , overflow:"auto"}}
+        >
           <div
             style={{
               padding: 24,
-              minHeight: 360,
+              minHeight: 433,
+              width:"1400px",
               background: colors.theme,
-              borderRadius: borderRadiusLG,
             }}
           >
-        cards section.
+            <CardsSection />
           </div>
         </Content>
-        <Footer
-          style={{
-            textAlign: 'center',
-          }}
-        >
-          Ant Design ©{new Date().getFullYear()} Created by Ant UED
-        </Footer>
       </Layout>
     </Layout>
   );
