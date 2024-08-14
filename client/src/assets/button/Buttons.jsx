7aded532e9e033fdf989261fc8817ef54bc92187
Sharyar Anwer <76113895+MUSHARIB-97@users.noreply.github.com>
2024-08-14 09:12:26 -0700

    initial commit for 42 files

---
+++ client/src/assets/button/Buttons.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Button } from 'antd';
+import colors from '../colors/color';
+export const Buttons = ({text,  onClick , className }) => {
+  return (
+    <div className='ant-btn-parrent'>
+
+
+
+<Button onClick={onClick}  className={className} style={{backgroundColor:colors.buttonColor}}>{text}</Button>
+
+    </div>
+  )
+}
---
+++ client/src/components/cardsSection/CardsSection.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { Cards } from "../../assets/cards/Cards";
+import icons from "../../assets/icons";
+
+
+export const CardsSection = () => {
+  const cardArray = [
+    {
+      cardId: 1,
+      cardName: "To do",
+    },
+    {
+      cardId: 2,
+
+      cardName: "card2",
+    },
+    {
+      cardId: 3,
+
+      cardName: "card3",
+    },
+    {
+      cardId: 4,
+
+      cardName: "Add Card",
+    },
+  ];
+  const dataArray = [
+    {
+      description: "task Description",
+    },
+    {
+      description: "task Description",
+    },
+    {
+      description: "task Description this is not enouigh to describe",
+    },
+  ];
+  const addCard = [
+    {
+      description: "task Description",
+    },
+   
+  ];
+  return (
+    <div className="card-Parrent">
+      <div className="card1 d-flex justify-content-between">
+      
+        {cardArray?.map((items)=>{
+            return(
+
+                <Cards data={items.cardName ==="Add Card" ? addCard :items.cardName=== "To do" ? dataArray:[]} cardsName={items.cardName} cardId={items.cardId}  icon={icons.editIcon} />
+            )
+        })}
+         
+      </div>
+    </div>
+  );
+};
