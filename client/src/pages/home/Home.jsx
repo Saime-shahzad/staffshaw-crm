8d6ee0b271bd11981520a5a27181e3ab6c94e79d
Sharyar Anwer <76113895+MUSHARIB-97@users.noreply.github.com>
2024-08-21 08:19:27 -0700

    minor changes

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,18 +1,17 @@
-import React from 'react'
-import Index from '../layout/Index'
-import { Header } from '../../components/header/Header'
+import React from "react";
+import Index from "../layout/Index";
+import { Header } from "../../components/header/Header";
 
-export const Home = () => {
+const Home = () => {
   return (
     <div>
-        <div>
-            <Header />
-        </div>
-    <div className='Layout-Parrent'>
-
+      <div>
+        <Header />
+      </div>
+      <div className="Layout-Parrent">
         <Index />
+      </div>
     </div>
-        
-        </div>
-  )
-}
+  );
+};
+export default Home;
