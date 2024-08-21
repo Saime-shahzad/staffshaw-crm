8b011b189429655563f39189e5510c987a9330ff
Sharyar Anwer <76113895+MUSHARIB-97@users.noreply.github.com>
2024-08-21 08:17:10 -0700

    toast added

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
-import { Home } from './pages/home/Home';
+import React from "react";
+import Home from "./pages/home/Home";
+import {  ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 
 function App() {
   return (
     <div className="">
-       
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
       <Home />
-
+      
     </div>
   );
 }
